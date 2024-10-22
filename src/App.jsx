a22f49b520790b662dec6a345cef974ea4a848ac
abubakkar-js-dev <mdabubakkars182@gmail.com>
2024-10-22 08:30:11 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl bg-purple-300 p-4 text-white text-center font-bold'>React with Tailwind Css</h1>
+    </>
+  )
+}
+
+export default App
